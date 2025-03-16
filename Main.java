f8dcc7180842e99723d9ba627ba1e9c8a2a32990
syatirazulaika <syatirazulaika@gmail.com>
2025-03-16 22:25:53 +0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+package Ekpedisi;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sytr = new Scanner(System.in);
+        
+        // Menampilkan informasi perusahaan logistik
+        EkspedisiBarang.displayInfoLogistik();
+        System.out.println();
+
+        // Input data dari user
+        System.out.print("Masukkan Nama Pengirim: ");
+        String nama = sytr.nextLine();
+        System.out.print("Masukkan Alamat Tujuan: ");
+        String alamat = sytr.nextLine();
+        System.out.print("Masukkan Berat Barang (kg): ");
+        double berat = sytr.nextDouble();
+        System.out.print("Masukkan Biaya Dasar: ");
+        double biayaDasar = sytr.nextDouble();
+
+        // Membuat objek dengan data inputan
+        EkspedisiBarang pengiriman = new EkspedisiBarang(nama, alamat, berat, biayaDasar);
+        pengiriman.displayInfoPengiriman();
+        System.out.println();
+
+        // Menguji Overloaded Method
+        System.out.print("Masukkan diskon persentase: ");
+        double diskon = sytr.nextDouble();
+        System.out.println("Ongkir setelah diskon " + diskon + "%: Rp " + pengiriman.hitungOngkir(diskon));
+        
+        System.out.print("Masukkan biaya tambahan: ");
+        double biayaTambahan = sytr.nextDouble();
+        System.out.println("Ongkir setelah diskon " + diskon + "% + biaya tambahan Rp " + biayaTambahan + ": Rp " + pengiriman.hitungOngkir(diskon, biayaTambahan));
+        
+        System.out.print("Masukkan jarak pengiriman (km): ");
+        int jarak = sytr.nextInt();
+        System.out.println("Ongkir untuk jarak " + jarak + " km: Rp " + pengiriman.hitungOngkir(jarak));
+        
+        sytr.close();
+    }
+}
